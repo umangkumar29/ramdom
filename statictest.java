a689fca4467de1d7c86e519c2b76af461ee50d4b
Umang Kumar <86261599+umangkumar29@users.noreply.github.com>
2024-01-19 23:29:21 +0530

    Add files via upload

--- statictest.java
+++ statictest.java
@@ -1,95 +1,91 @@
-import java.util.ArrayList;
-import java.util.List;
-
-public class statictest {
+public class LibraryTest {
 
     public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-        Ingredient vodka = new Ingredient("Vodka", "Clear");
-        Ingredient orangeJuice = new Ingredient("Orange Juice", "Orange");
-        Ingredient grenadine = new Ingredient("Grenadine", "Red");
+        Book book1 = new Book("The Catcher in the Rye", "J.D. Salinger", 250, "Fiction");
 
-        Bartender bartender = new Bartender();
+        Magazine magazine1 = new Magazine("National Geographic", "January 2024", "Science and Nature");
 
-        bartender.addIngredient(vodka);
-        bartender.addIngredient(orangeJuice);
-        bartender.addIngredient(grenadine);
+        Librarian librarian = new Librarian();
 
-        Cocktail sunriseCocktail = bartender.mixCocktail("Sunrise Cocktail");
+        Reader reader = new Reader("John Doe", 25);
 
-        System.out.println("Cocktail: " + sunriseCocktail);
+        System.out.println("Book 1: " + book1);
+        System.out.println("Magazine 1: " + magazine1);
+
+        librarian.checkOutBook(reader, book1);
+        librarian.checkOutMagazine(reader, magazine1);
+
+        librarian.returnBook(reader, book1);
+        librarian.returnMagazine(reader, magazine1);
     }
 }
 
-class Bartender {
-
-    private List<Ingredient> availableIngredients = new ArrayList<>();
-
-    public void addIngredient(Ingredient ingredient) {
-        availableIngredients.add(ingredient);
-    }
-
-    public Cocktail mixCocktail(String cocktailName) {
-        System.out.println("Mixing a " + cocktailName + "...");
-        Cocktail cocktail = new Cocktail(cocktailName);
-
-        // Assume a specific recipe for the Sunrise Cocktail
-        cocktail.addIngredient(findIngredient("Vodka"));
-        cocktail.addIngredient(findIngredient("Orange Juice"));
-        cocktail.addIngredient(findIngredient("Grenadine"));
-
-        return cocktail;
-    }
-
-    private Ingredient findIngredient(String ingredientName) {
-        for (Ingredient ingredient : availableIngredients) {
-            if (ingredient.getName().equalsIgnoreCase(ingredientName)) {
-                return ingredient;
-            }
-        }
-        return null; // Handle if ingredient is not found
-    }
+class Library {
+    // Assume there are common attributes and methods for a library
 }
 
-class Ingredient {
+class Book {
+    String title;
+    String author;
+    int pageCount;
+    String genre;
 
-    private String name;
-    private String color;
-
-    public Ingredient(String name, String color) {
-        this.name = name;
-        this.color = color;
+    public Book(String title, String author, int pageCount, String genre) {
+        this.title = title;
+        this.author = author;
+        this.pageCount = pageCount;
+        this.genre = genre;
     }
-
-    public String getName() {
-        return name;
-    }
-
-    // Other methods related to ingredients
 
     @Override
     public String toString() {
-        return "Ingredient [name=" + name + ", color=" + color + "]";
+        return "Book [title=" + title + ", author=" + author + ", pageCount=" + pageCount + ", genre=" + genre + "]";
     }
 }
 
-class Cocktail {
+class Magazine {
+    String name;
+    String issueDate;
+    String category;
 
-    private String name;
-    private List<Ingredient> ingredients = new ArrayList<>();
-
-    public Cocktail(String name) {
+    public Magazine(String name, String issueDate, String category) {
         this.name = name;
+        this.issueDate = issueDate;
+        this.category = category;
     }
-
-    public void addIngredient(Ingredient ingredient) {
-        ingredients.add(ingredient);
-    }
-
-    // Other methods related to cocktails
 
     @Override
     public String toString() {
-        return "Cocktail [name=" + name + ", ingredients=" + ingredients + "]";
+        return "Magazine [name=" + name + ", issueDate=" + issueDate + ", category=" + category + "]";
+    }
+}
+
+class Librarian {
+    void checkOutBook(Reader reader, Book book) {
+        System.out.println("Librarian checking out book: " + book.title + " for reader: " + reader.name);
+    }
+
+    void returnBook(Reader reader, Book book) {
+        System.out.println("Librarian receiving returned book: " + book.title + " from reader: " + reader.name);
+    }
+
+    void checkOutMagazine(Reader reader, Magazine magazine) {
+        System.out.println("Librarian checking out magazine: " + magazine.name + " for reader: " + reader.name);
+    }
+
+    void returnMagazine(Reader reader, Magazine magazine) {
+        System.out.println("Librarian receiving returned magazine: " + magazine.name + " from reader: " + reader.name);
+    }
+}
+
+class Reader {
+    String name;
+    int age;
+
+    public Reader(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 }
